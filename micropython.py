d78b559e1c7b5c77ffe3f23941bce573341dfec6
gibbsjoh <gibbsjoh@gmail.com>
2026-06-24 07:27:48 +0100

    Update micropython.py
    added sh1106 oled setup

--- micropython.py
+++ micropython.py
@@ -22,5 +22,33 @@ lcd.move_to(0, 0)
 lcd.putstr("Welcome!")
 
 # ^^ this works on ESP32. Haven't checked on Pi Pico but should work with relevant pins
+# ****************************************************************************************
 
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+# ****************************************************************************************
+# setup sh1106 or similar OLED over i2c
+# requires sh1106.py from https://github.com/robert-hh/SH1106
+# ** Imports:
+from machine import Pin, I2C
+import sh1106
+
+# Define the OLED I2C address and dimensions
+oledW = 128
+oledH = 64
+oledAddress = 0x3c
+i2c = I2C(1, sda=Pin(21), scl=Pin(22))
+oledDisplay = sh1106.SH1106_I2C(oledW, oledH, i2c, None, oledAddress, rotate=0, delay=0)
+oledDisplay.init_display()
+
+# the .text function takes the string, x postion, y position and optionally colour as arguments
+# show a welcome message
+oledDisplay.text('Welcome', 0, 0, 1)
+oledDisplay.text('to the', 0, 10, 1)
+oledDisplay.text('Jungle', 0, 19, 1)
+oledDisplay.show()
+# to clear, use this:
+oledDisplay.fill(0)
+
+# ^^ this works on ESP32. Haven't checked on Pi Pico but should work with relevant pins
 # ****************************************************************************************
